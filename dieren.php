c250e7792c580d7c739c5b9cf48331bb595d9ec7
marwan <marwan0320@live.nl>
2020-03-27 15:38:20 +0100

    eerste versie

--- dieren.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-printer_r($_POST['dieren']);
-foreach ($_POST['dieren'] as $dier){
-    echo"<img src='img/" . $dier .".jpg'>";
-}
--- functie's.php
+++
@@ -1,9 +0,0 @@
-<?php
-/** Functie by created PHPStorm. ... */
-function telop($a,$b){
-
-    $c=$a + $b;
-  echo $a. '+'.$b. "=".$c;
-}
-telop(12,45);
-
