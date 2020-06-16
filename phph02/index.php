b420a09574bff41cc4425fa29e94d9510b36f5e0
marwan <marwan0320@live.nl>
2020-06-16 22:04:04 +0200

    h02

---
+++ phph02/index.php
@@ -0,0 +1,40 @@
+<?php
+
+$a= "Ik heb niet gefaald";
+$b=". Ik heb alleen ";
+$aantal=" 10000 manieren gevonden die niet werken. ";
+$naam ="-Thomas Edison ";
+
+echo $a.$b.$aantal.$naam;
+
+echo "<br>";
+echo "<br>";
+//
+$a= "die geen fout";
+$b= "en";
+$c= " niet.";
+
+echo "Mensen ".$a.$b." maken, werken".$c."-Albert Plesman (KLM)";
+echo "<br>";
+echo "<br>";
+//
+$a = "ry";
+$b = ", but";
+$c = "fail";
+
+echo "T".$a." and ".$b." never ".$c." try.";
+echo "-Tyra Banks";
+echo "<br>";
+echo "<br>";
+
+$a = "omgaan";
+$b = "met ";
+$c = "teleu";
+
+echo "De cursus ".$a." omgaan ".$b.$c. "stellingen "."kan vanavond helaas niet doorgaan.";
+echo "- Herman Finkers";
+
+
+
+
+?>
